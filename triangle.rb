ef866568bd9861449ad49094dab92af13c287730
NikitaSharipov <sharipovkzn@mail.ru>
2018-07-18 13:32:31 +0300

    Исправления

--- triangle.rb
+++ triangle.rb
@@ -3,4 +3,4 @@ base = gets.chomp
 puts "Введите пожалуйста высоту треугольника"
 height = gets.chomp
 
-puts "Площадь треугольника равна #{0.5*base.to_i*height.to_i}"
+puts "Площадь треугольника равна #{0.5 * base.to_f * height.to_f}"
